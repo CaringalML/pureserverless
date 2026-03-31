de2461422213e3c07fe86cb3a4a8c9f1de79042f
Martin Lawrence M. Caringal <75959399+Martin-Caringal-172397@users.noreply.github.com>
2026-04-01 01:58:29 +1300

    Reorganize terraform-state: split main.tf into versions, provider, s3, dynamodb

--- terraform-state/s3.tf
+++ terraform-state/s3.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.6.0"
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-# -------------------------------------------------------
-# S3 Bucket for Terraform Remote State
-# -------------------------------------------------------
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.state_bucket_name
 
@@ -53,23 +36,4 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-# -------------------------------------------------------
-# DynamoDB Table for Terraform State Locking
-# -------------------------------------------------------
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = var.lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name    = "Terraform State Lock Table"
-    Purpose = "terraform-state-locking"
-  }
 }
