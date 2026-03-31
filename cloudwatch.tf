99d848eb204dd4b878750539e5af55fa368b96b8
Martin Lawrence M. Caringal <75959399+Martin-Caringal-172397@users.noreply.github.com>
2026-04-01 02:01:53 +1300

    Import existing CloudWatch log group to fix ResourceAlreadyExistsException

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,3 +1,10 @@
+# Imports the log group if Lambda already auto-created it on first invocation.
+# Once it's in state this block is a no-op on subsequent applies.
+import {
+  to = aws_cloudwatch_log_group.lambda
+  id = "/aws/lambda/${var.lambda_function_name}-${var.environment}"
+}
+
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${var.lambda_function_name}-${var.environment}"
   retention_in_days = 14
